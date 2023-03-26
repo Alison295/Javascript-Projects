65bbebff8e8eceda4b453686167330cd92fd245e
Alison295 <56089606+Alison295@users.noreply.github.com>
2023-03-26 22:18:58 +0100

    Add files via upload
    Pushing Basic Javascript Projects to repository

---
+++ Javascript Projects/Basic Javascript Projects/Projects1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+window.alert("Hello, World!"); // creating a pop-up box using window.alert method
+
+var showName = "Call the Midwife <br>"
+document.write(showName); // assigning variable with string value and using document.write method to display it
+
+document.write("\"If you set your goals ridiculously high and it\'s a failure," + " you will fail above everyone\'s else\'s success\"" + " - James Cameron <br>");
+// writing a string that contains apostrophes and concatenating them together
+
+sen1 = "Toyota"
+sen2 = "The Best Built Cars in the World <br>"
+document.write(sen1 + sen2);
+//assigning two variable values and concatenating them together
+
+var movie = "Finding Nemo", Protagonist = "Marlin", Deuteragonist = "Dory",
+Tritagonist = "Nemo", Antagonist = "Darla";
+document.write(Protagonist)
+// assigning multiple variables 
+
+document.write(3 * 5);// an expression
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}// an event handlier that displays the date when the button is clicked 
