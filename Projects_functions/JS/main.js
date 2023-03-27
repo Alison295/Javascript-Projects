c0d7f24cf90428e39330a5b24ad9029b87bf7bb8
Alison295 <56089606+Alison295@users.noreply.github.com>
2023-03-27 19:11:16 +0100

    Add files via upload

---
+++ Projects_functions/JS/main.js
@@ -0,0 +1,9 @@
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}// an event handlier that displays the date when the button is clicked 
+
+function myFunction() {
+    var quote = "Do or Do not";
+    quote += "  There is no try";
+    document.getElementById("yoda").innerHTML = quote;
+}
